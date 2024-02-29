4f824be8da96415447650cb505f42bf10096e902
Gaurav Sablok <sablokg@gmail.com>
2024-02-29 15:58:17 +0100

    Update gfareader.py

--- src/graphanalyzer/gfareader.py
+++ src/graphanalyzer/gfareader.py
@@ -2,19 +2,19 @@
 # Universitat Potsdam
 # Author Gaurav Sablok
 # date: 2024-2-29
-def gfaformatter(gfafile):
-    import pandas as pd
-    import seaborn as sns
-    import matplotlib as plt
-    import re
-    if gfafile is not None:
-        gfa = gfafile
-        # add the array of the columnames
-        columnnames = []
-        readgfa = pd.read_csv([line.split("\t") for line in open(gfa)], columns = columnsnames)
-        sequencematch = []
-        for i in readgfa.iterrows():
-            for j in range(len(["A", "T", "G", "C"])):
-                if j in i:
-                    sequencematch.append(i)
-        print(f"the number of the sequence elements in the gfa are: len(sequencematch)")
+def gfafastawrite(gfafile, filewrite):
+    """
+    a gfa to fasta write for writing the graph connections for the 
+    GFA files
+    """
+    sort = list(pd.DataFrame([line.split("\t") for line in open("/home/gaurav/Downloads/MT.gfa")], \
+                                columns = ["a","b","c","d","e","f","g"])["c"])
+    indices = [i for i in range(len(sort)) if "+" not in sort[i] and "-" not in sort[i]]
+    sequences = [i for i in sort if "+" not in i and "-" not in i]
+    ids = list(pd.DataFrame([line.split("\t") for line in open("/home/gaurav/Downloads/MT.gfa")], \
+                            columns = ["a","b","c","d","e","f","g"])["b"])[0:len(indices)]
+    with open(filewrite, "w") as fastawrite:
+        for i in range(len(ids)):
+            fastawrite.write(">"+ids[i])
+            fastawrite.write(sequences[i])
+    fastawrite.close()
